472ce156e3d5c6e5313daa022ee7eb98a7b6af9c
Dnworkf <general-n-dnmf@outlook.com>
2023-07-13 11:38:04 +0300

    polish

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import "./index.html";
 import "./index.scss";
 import "./scripts/animations";
-
-const temp = function (arg1, arg2) {
-  for (let i = 0; i <= arg1.length; i++) {
-    console.log("asffsafs");
-  }
-  return 0;
-};
