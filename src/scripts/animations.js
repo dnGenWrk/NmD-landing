005efe62b1a65bd8b6d2c65586b079c26e8d4939
Dnworkf <general-n-dnmf@outlook.com>
2023-07-13 23:30:44 +0300

    remove logging from animations.js

--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -1,28 +1,22 @@
 const chooseAnimation = function (listClasses) {
   for (let val of listClasses) {
     if (val.includes("from-left-animation")) {
-      console.log("HAS CLASS from-left-animation");
       return "animation_fade-from-left";
     }
     if (val.includes("from-right-animation")) {
-      console.log("HAS CLASS from-right-animation");
       return "animation_fade-from-right";
     }
 
     if (val.includes("from-top-animation")) {
-      console.log("HAS CLASS from-top-animation");
       return "animation_fade-from-top";
     }
     if (val.includes("from-bottom-animation")) {
-      console.log("HAS CLASS from-bottom-animation");
       return "animation_fade-from-bottom";
     }
   }
 };
 const callback = function (entries) {
   entries.forEach((entry) => {
-    console.log(entry);
-
     if (entry.isIntersecting) {
       let trgtLists = entry.target.classList;
       let selectedAnimation = chooseAnimation(trgtLists);
